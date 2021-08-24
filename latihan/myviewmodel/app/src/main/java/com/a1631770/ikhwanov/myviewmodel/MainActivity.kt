ab4b3d7abbefb47bf11202fc613a0dc8bd9063f1
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-08-24 11:55:07 +0800

    create myviewmodel

---
+++ latihan/myviewmodel/app/src/main/java/com/a1631770/ikhwanov/myviewmodel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a1631770.ikhwanov.myviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
